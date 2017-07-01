cfa619a4c40195702d483b71ee1d5aa2776782ed
bando-shintaro <bando142141@gmail.com>
2017-07-01 11:15:08 +0900

    add Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-FROM golang:1.8.3 as builder
+FROM golang:1.8.3-alpine as builder
 MAINTAINER bando-shintaro<bando142141@gmail.com>
 
+RUN apk update
+RUN apk upgrade
+RUN apk --no-cache add git musl-dev
+RUN go get -u github.com/tools/godep
 COPY . /go/src/api-server
 WORKDIR /go/src/api-server
-RUN go get -u github.com/tools/godep
+
 RUN godep get
 RUN godep go build
 
 FROM alpine:latest
-COPY --from=builder /go/src/api-server/api-server  .
-CMD ["./api-server"]
-
+COPY --from=builder /go/src/api-server/api-server  /hermosa/api-server
+CMD ["/hermosa/api-server"]
--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
-	//	"github.com/jinzhu/gorm"
-	//	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"net/http"
 )
+
+type Gene struct {
+	gorm.Model
+	Id    int
+	Param int
+}
 
 func main() {
 	fmt.Println("vim-go")
+	db, _ := gorm.Open("mysql", "root:api@localhost/api?charset=utf8&parseTime=True&loc=Local")
+
+	defer db.Close()
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
 }
